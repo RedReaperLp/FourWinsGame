5ee2cd4540d89221b79224838f556f468fed2e8d
Raphael Kuschmierz <fiffykuschmierz@gmail.com>
2022-11-18 19:27:56 +0100

    Auto Update

--- src/main/java/com/github/redreaperlp/fourwinsserver/Main.java
+++ src/main/java/com/github/redreaperlp/fourwinsserver/Main.java
@@ -5,13 +5,20 @@ import com.github.redreaperlp.fourwinsserver.objects.Config;
 import com.github.redreaperlp.fourwinsserver.objects.enums.ConfigType;
 import com.github.redreaperlp.fourwinsserver.server.InitServer;
 
-import java.io.Console;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.sql.Time;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static Config config = new Config();
     boolean isServer = false;
     public static boolean wantColoredConsole = false;
+    public int localeVersion = 100;
 
 
     /**
@@ -19,21 +26,79 @@ public class Main {
      *
      * @param args depending on the arguments, the program will start as a server or a client
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        File update = new File("update.bat");
+        if (update.exists()) {
+            update.delete();
+        }
+        String[] line;
+        String downloadUrl = "";
+        int version = 0;
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL("https://api.github.com/repos/RedReaperLP/FourWinsGame/releases/latest").openConnection();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            line = reader.readLine().split(",");
+            reader.close();
+            for (String check : line) {
+                if (check.contains("\"browser_download_url\":")) {
+                    check = check.replace("\"browser_download_url\":", "");
+                    check = check.replace("\"", "");
+                    check = check.replace("}", "");
+                    check = check.replace("]", "");
+                    downloadUrl = check;
+                }
+                if (check.contains("\"tag_name\":")) {
+                    check = check.replace("\"tag_name\":", "");
+                    check = check.replace("\"", "");
+                    version = Integer.parseInt(check.replace(".", ""));
+                }
+            }
+        } catch (IOException e) {
+        }
+
         Main main = new Main();
-        switch (args[0].toLowerCase(Locale.ROOT)) {
-            case "-server":
-                main.isServer = true;
-                main.checkConfig();
-                wantColoredConsole = Boolean.parseBoolean(Main.config.getConfig(ConfigType.WANT_COLORED_CONSOLE.value()));
-                new InitServer().init(Integer.parseInt(config.getConfig(ConfigType.SERVER_PORT.value())),
-                        Integer.parseInt(config.getConfig(ConfigType.MAX_GAMES.value())));
-                break;
-            case "-client":
-                main.checkConfig();
-                wantColoredConsole = Boolean.parseBoolean(Main.config.getConfig(ConfigType.WANT_COLORED_CONSOLE.value()));
-                new Client();
-                break;
+        if (main.localeVersion >= version) {
+            switch (args[0].toLowerCase(Locale.ROOT)) {
+                case "-server":
+                    main.isServer = true;
+                    main.checkConfig();
+                    wantColoredConsole = Boolean.parseBoolean(Main.config.getConfig(ConfigType.WANT_COLORED_CONSOLE.value()));
+                    new InitServer().init(Integer.parseInt(config.getConfig(ConfigType.SERVER_PORT.value())),
+                            Integer.parseInt(config.getConfig(ConfigType.MAX_GAMES.value())));
+                    break;
+                case "-client":
+                    main.checkConfig();
+                    wantColoredConsole = Boolean.parseBoolean(Main.config.getConfig(ConfigType.WANT_COLORED_CONSOLE.value()));
+                    new Client();
+                    break;
+            }
+        } else {
+            try {
+                String versionString = "";
+                for (char c : String.valueOf(version).toCharArray()) {
+                    versionString = versionString + c + ".";
+                }
+                System.out.println("A new version is available: " + versionString.substring(0, versionString.length() - 1));
+                String path = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getPath();
+                String fileName = path.substring(path.lastIndexOf("\\") + 1);
+                TimeUnit.SECONDS.sleep(2);
+                URL url = new URL(downloadUrl);
+                File file = new File("update.bat");
+                FileOutputStream fos = new FileOutputStream(file);
+                DataOutputStream dos = new DataOutputStream(fos);
+                dos.writeBytes("bitsadmin.exe /transfer \"Update\" " + downloadUrl + " " + System.getProperty("user.dir") + "\\" + fileName);
+                dos.writeBytes("\njava -jar " + fileName + " " + args[0]);
+                dos.writeBytes("\n");
+                dos.close();
+                Process process = Runtime.getRuntime().exec("cmd /c start update.bat");
+                System.exit(0);
+            } catch (MalformedURLException e) {
+            } catch (IOException e) {
+                e.printStackTrace();
+                TimeUnit.SECONDS.sleep(5);
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
